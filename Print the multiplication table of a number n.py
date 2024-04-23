c47ef27914e3c76fd7ccd160736f21b367a44644
Wazid Khan <wazid0853@gmail.com>
2024-04-23 23:48:04 +0600

    [Update 1.1.5]

--- Print the multiplication table of a number n.py
+++ Print the multiplication table of a number n.py
@@ -1,12 +1,12 @@
 # Print the multiplication table of a number n.
-loop =30
+loop = 0
+start = 1
 distribute = 1 
-while distribute == loop :
+while distribute >= loop :
         n = int(input("Input a Number ----->> "))
         range = int(input("Input Range  ----->> "))
         print(str(distribute) + " of " + str(loop))
-        start = 1
-        distribute =+ 1 
+        distribute +=  1 
         while start<=range :
             multiplication = n*start
             print(str(n) + " * " + str(start) + " = " + str(multiplication) )
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 loop =30
 distribute = 1 
-while distribute == loop :
-        n = int(input("Input a Number ----->> "))
-        range = int(input("Input Range  ----->> "))
-        print(str(distribute) + " of " + str(loop))
-        start = 1
-        distribute =+ 1 
+while distribute <= loop :
+    print(distribute)
+    distribute = distribute + 1
