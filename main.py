d1abf9387ee528f84faebd244091fb50512af287
Wazid Khan <wazid0853@gmail.com>
2024-04-23 11:20:42 +0600

    [Python Project update]

---
+++ main.py
@@ -0,0 +1,24 @@
+f_name = "Wazid"
+student ={
+    "name": 
+    {
+        "first_name": f_name,
+        "last_name": "Doe"
+    },
+    "address":
+    {
+        "city": "New York",
+        "state": "NY"
+    },
+    "phone":
+    {
+        "home": "212-555-1234",
+        "mobile": "646-555-4567"
+    },
+    "email": "Wazidkhan@gamil.com",
+    "gender": "Male" ,
+    "age": 25
+}
+
+print(student["phone"]["home"])
+print(student["phone"]["mobile"])
